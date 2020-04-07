59d2b701d0142426147260da20cc80fb61c38326
Willy Huang <willy303156@gmail.com>
2020-04-08 00:32:48 +0800

    "ver 2"

--- 5_5_Simple_Ticker/main.cpp
+++ 5_5_Simple_Ticker/main.cpp
@@ -1,17 +1,21 @@
 #include "mbed.h"
 
 Ticker time_up;
-DigitalOut redLED(LED1);
-DigitalIn SW(SW3);
+DigitalOut blueLED(LED_BLUE);
+DigitalIn switch1(SW2);
+DigitalIn switch2(SW3);
 void blink(){
-    redLED = !redLED;
+    blueLED = !blueLED;
 }
 
 int main(){
-    time_up.attach( &blink, 1 );
+    time_up.attach( &blink, 0.5f );
     while(1)
     {
-        if (SW == 0)
+        if (switch2 == 0)
             time_up.detach();
+        if (switch1 == 0)
+            time_up.attach( &blink, 0.5f );
+        
     }
 }
